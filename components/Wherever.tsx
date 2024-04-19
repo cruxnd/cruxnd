eafb08c057abdb960c2aa2038d16545df47ae869
RalphFred <raphaelgbaorun@gmail.com>
2024-04-19 04:42:08 +0100

    build: worked on the Wherever section

--- components/Wherever.tsx
+++ components/Wherever.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export default function Wherever() {
   return (
-    <div className="lg:px-24 lg:py-12 flex flex-col lg:flex-row">
+    <div className="lg:px-24 lg:py-12 flex flex-col lg:flex-row items-center justify-between">
       <div>
         <Image
           src="/images/earth.png"
           alt="bulb"
           width={1000}
           height={1000}
-          className="w-[400px]"
+          className="w-[500px]"
         />
       </div>
-      <h2>
-        Wherever you are on the planet, <br />
+      <h2 className="text-4xl font-semibold">
+        Wherever <span className="text-gold-1">YOU</span> are on the planet, <br />
         Distance doesn't really matter here. <br />
         We are a second away!
       </h2>
