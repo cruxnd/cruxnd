e9e2c0d07dcb94d9af57b0e220f6fdb96046911f
RalphFred <raphaelgbaorun@gmail.com>
2024-05-04 10:48:56 +0100

    fix: worked on the AboutHero section

--- components/AboutHero.tsx
+++ components/AboutHero.tsx
@@ -1,25 +1,23 @@
 import Image from "next/image";
 
 export default function AboutHero() {
-  return(
-    <div className="px-6 md:px-12 lg:px-24 lg:py-20 h-[calc(100vh-100px)] relative flex items-center justify-center">
-      
-
-      <div className="absolute top-[150px] left-[200px] w-[250px] px-4 py-6 border border-gold-1 bg-[#f1e3c0] rounded-full text-center text-sm">
+  return (
+    <div className="px-6 md:px-12 lg:px-24 lg:py-20 h-[calc(100vh-100px)] relative flex items-center justify-center bg-[url('/images/herobg.jpg')]">
+      <div className="absolute top-[150px] left-[200px] px-12 py-6 border-2 rounded-full text-center text-xl">
         Who are we?
       </div>
 
-      <div className="absolute top-20 right-[300px] w-[250px] px-4 py-6 border border-blue-400 bg-[#b4fafd] rounded-full  text-center text-sm">
+      <div className="absolute top-20 right-[300px] px-12 py-6 border-2  rounded-full  text-center text-xl">
         What do we do?
       </div>
 
-      <div className="absolute bottom-[250px] right-28 w-[250px] px-4 py-6 border border-green-400 bg-[#a2f38b] rounded-full  text-center text-sm">
+      <div className="absolute bottom-[250px] right-28 px-12 py-6 border-2  rounded-full  text-center text-xl">
         What do we stand for?
       </div>
 
-      <div className="absolute bottom-[180px] left-28 w-[250px] px-4 py-6 border border-purple-400 bg-[#cb91ee] rounded-full  text-center text-sm">
-        
+      <div className="absolute bottom-[180px] left-28  px-12 py-6 border-2  rounded-full  text-center text-xl">
+        Who do we work with?
       </div>
     </div>
-  )
+  );
 }
