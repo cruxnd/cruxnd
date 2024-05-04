29bd4d09fff0642a32545e94fe6def1a0a3a0e57
RalphFred <raphaelgbaorun@gmail.com>
2024-05-04 11:01:20 +0100

    build: Made the About page mobile responsive

--- components/AboutHero.tsx
+++ components/AboutHero.tsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 
 export default function AboutHero() {
   return (
-    <div className="px-6 md:px-12 lg:px-24 lg:py-20 h-[calc(100vh-100px)] relative flex items-center justify-center bg-[url('/images/herobg.jpg')]">
-      <div className="absolute top-[150px] left-[200px] px-12 py-6 border-2 rounded-full text-center text-xl">
+    <div className="px-6 md:px-12 lg:px-24 lg:py-20 h-[calc(100vh-85px)] relative flex items-center justify-center bg-[url('/images/herobg.jpg')]">
+      <div className="absolute top-12 lg:top-[150px] left-6 lg:left-[200px] px-12 py-6 border-2 rounded-full text-center text-sm lg:text-xl">
         Who are we?
       </div>
 
-      <div className="absolute top-20 right-[300px] px-12 py-6 border-2  rounded-full  text-center text-xl">
+      <div className="absolute top-[180px] lg:top-20 right-12 lg:right-[300px] px-12 py-6 border-2  rounded-full text-center text-sm lg:text-xl">
         What do we do?
       </div>
 
-      <div className="absolute bottom-[250px] right-28 px-12 py-6 border-2  rounded-full  text-center text-xl">
+      <div className="absolute bottom-12 lg:bottom-[250px] right-12  lg:right-28 px-12 py-6 border-2  rounded-full  text-center text-sm lg:text-xl">
         What do we stand for?
       </div>
 
-      <div className="absolute bottom-[180px] left-28  px-12 py-6 border-2  rounded-full  text-center text-xl">
+      <div className="absolute bottom-[180px] left-12 lg:left-28  px-12 py-6 border-2  rounded-full  text-center text-sm lg:text-xl">
         Who do we work with?
       </div>
     </div>
