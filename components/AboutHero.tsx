ddd527f5c443b1c537eab2502c74a5ebae461f51
RalphFred <raphaelgbaorun@gmail.com>
2024-05-12 16:14:33 +0100

    Worked on the FAq

--- components/AboutHero.tsx
+++ components/AboutHero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function AboutHero() {
   return (
-    <div className="px-6 md:px-12 lg:px-24 lg:py-20 h-[calc(100vh-85px)] relative flex items-center justify-center bg-[url('/images/herobg.jpg')]">
+    <div className="px-6 md:px-12 lg:px-24 lg:py-20 h-[calc(100vh-85px)] relative flex items-center justify-center bg-[url('/images/bg.svg')] bg-no-repeat bg-cover">
       <div className="absolute top-12 lg:top-[150px] left-6 lg:left-[200px] px-12 py-6 border-2 md:border-4 lg:border-2 hover:font-semibold cursor-pointer rounded-full text-center text-sm md:text-3xl lg:text-xl">
         Who are we?
       </div>
--- components/Faqs.tsx
+++ components/Faqs.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
-
 
 interface FaqItem {
   question: string;
@@ -19,31 +19,70 @@ const faqData: FaqItem[] = [
     answer:
       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
   },
+  {
+    question: "What is Lorem Ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    question: "Why do we use it?",
+    answer:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+  },
+  {
+    question: "What is Lorem Ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    question: "Why do we use it?",
+    answer:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+  },
+  {
+    question: "What is Lorem Ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
 ];
 
 export default function Faqs() {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   const toggleAnswer = (index: number) => {
-    setOpenIndex((prevIndex: number | null) => (prevIndex === index ? null : index));
+    setOpenIndex((prevIndex: number | null) =>
+      prevIndex === index ? null : index
+    );
   };
 
   return (
-    <div className="px-6 md:px-12 lg:px-24 lg:py-20">
-      <h1 className="text-3xl font-bold mb-4">Frequently Asked Questions</h1>
-      <div className="">
-        {faqData.map((item, index) => (
-          <div
-            key={index}
-            className="border-b pb-4"
-            onClick={() => toggleAnswer(index)}
-          >
-            <div className="font-semibold">{item.question}</div>
-            <div className="mt-2">
-              {openIndex === index && <div className="mt-2">{item.answer}</div>}
+    <div className="px-6 md:px-12 lg:px-24 lg:py-20 flex flex-col lg:flex-row">
+      <div className="lg:w-1/2 lg:mr-20">
+        <h1 className="text-3xl font-bold mb-4">Frequently Asked Questions</h1>
+        <div className="">
+          {faqData.map((item, index) => (
+            <div
+              key={index}
+              className="border-b pb-4"
+              onClick={() => toggleAnswer(index)}
+            >
+              <div className="font-semibold">{item.question}</div>
+              <div className="mt-2">
+                {openIndex === index && (
+                  <div className="mt-2">{item.answer}</div>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+      </div>
+      <div className="w-1/2">
+      <Image 
+        src="/images/questions.jpg"
+        alt="Questions"
+        width={500}
+        height={500}
+      />
       </div>
     </div>
   );
